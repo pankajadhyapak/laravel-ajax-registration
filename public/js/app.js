1075db25dcc6b628ea363822088336fa88365f1a
Pankaj Adhyapak <pankaj@Pankajs-MacBook-Air.local>
2015-10-20 22:29:14 +0530

    Basic Setup and Layouts

---
+++ public/js/app.js
@@ -0,0 +1,41 @@
+$( "#registrationForm" ).submit(function(e) {
+    e.preventDefault();
+
+    //For Laravel CSRF Token
+    $.ajaxSetup({ headers: {'X-CSRF-TOKEN': $('#token').val()} });
+
+    var $form = $(this),
+        data = $form.serialize(),
+        url = $form.attr("action");
+
+    var posting = $.post( url, { formData: data});
+
+    posting.done(function( data ) {
+
+        //clear any error message if exists
+        $('.help-block').html("");
+
+        //remove error class
+        $('.form-group').removeClass('has-error');
+
+        //if validation failed
+        if(data.fail) {
+            $.each(data.messages, function( index, value ) {
+                var errorDiv = '#'+index;
+                $(errorDiv).parent().addClass('has-error');
+                $(errorDiv).next().html(value);
+            });
+        }
+
+        //if registration is success
+        if(data.success) {
+            $('#registrationForm').fadeOut(function(){
+                var successContent = '<div class="alert alert-success">' +
+                    '<h3>Registration Completed Successfully</h3>' +
+                    '<h4>You Can Login here <a href="#">Login</a> </div>';
+                $('.successMessage').html(successContent);
+            });
+
+        } //success
+    }); //done
+});
